2e757e0e50a9b5e8dfbbf9f88d801c0ddfec6759
Houssame Rahiche <houssamerahiche@gmail.com>
2022-07-15 11:27:30 +0100

    Adding workout form and handle submit

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 // components:
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutsForm from '../components/WorkoutsForm'
 
 const Home = () => {
 
@@ -25,6 +26,7 @@ const Home = () => {
                 return <WorkoutDetails workout={workout} />
             })}
         </div>
+        <WorkoutsForm />
     </div>
   )
 }
